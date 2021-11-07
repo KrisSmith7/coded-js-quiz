1aabafa5adfaebec73c752b96eaf9133589d0eb5
KrisSmith7 <krissmithcodes@gmail.com>
2021-11-07 00:47:26 -0400

    added high score variables

--- assets/script.js
+++ assets/script.js
@@ -6,16 +6,33 @@ const answerSet = document.getElementById("answer-buttons");
 
 const timerEl = document.getElementById("countdown");
 
+var highscore = []
+
+var showHighScores = function () {
+    var highScoreList = document.getElementById("score-field");
+    var playerInfo = document.createElement("li");
+           playerInfo.className = "player-score";
+             playerInfo.innerText = prompt("Enter your name:");
+            highScoreList.appendChild(playerInfo);
+            function setScores () {localStorage.setItem (playerInfo, highscore)};
+            function getScores () {JSON.parse(window.localStorage.getItem(playerInfo, highscores)) || []};
+        }
+
+
 var counter = 60;
 
 const showTimer = function () {
+    // WHEN all questions are answered or the timer reaches 0
+    // THEN the game is over
     function endgame() {
         clearInterval(timer);
+        getScores();
+        setScores ();
     }
     function countdown() {
         counter--;
         timerEl.innerText = "Time Left: " + counter;
-        if (counter <= 0) { endgame(); }
+        if (counter <= 0) {timerEl.innerHTML = "<h3>'Game Over'</h3>"; endgame(); }
     }
     var timer = setInterval(countdown, 1000);
 };
@@ -26,19 +43,19 @@ const questions = [
         question: "Which built-in method calls a function for each element in the array?",
         answers: [
             { text: "A - forEach()", correct: "true"},
-            { text: "B - while()", correct: false },
-            { text: "C - loop()", correct: false },
-            { text: "D - array()", correct: false },
+            { text: "B - while()", correct: "false" },
+            { text: "C - loop()", correct: "false" },
+            { text: "D - array()", correct: "false" },
         ]
     },
 
     {
         question: "Which built-in method returns the calling string value converted to upper case?",
         answers: [
-            { text: "A - changeCase(case)", correct: false },
-            { text: "B - toUpper()", correct: false },
-            { text: "C - toUpperCase()", correct: true },
-            { text: "D - allCase(upper)", correct: false },
+            { text: "A - changeCase(case)", correct: "false" },
+            { text: "B - toUpper()", correct: "false" },
+            { text: "C - toUpperCase()", correct: "true" },
+            { text: "D - allCase(upper)", correct: "false" },
         ]
     },
 ]
@@ -57,58 +74,49 @@ var questionCounter = 0
              answerSet.appendChild(answerButton);
              console.dir(answerButton)
              function checkAnswers () {
-                 if (answerButton.title == true) {questionText.innerText = "YES!"}
+                 if (answerButton.title == true) {questionText.innerText = "YES!"; }
                 //  WHEN I answer a question incorrectly
                 //  THEN time is subtracted from the clock
                  else if (answerButton.title == false) {questionText.innerText = "Incorrect! " + "Next question...";
                 counter = counter - 10;}
-             }
+             };
              answerButton.addEventListener('click', checkAnswers)
-            }
+            };
         };
-       
 
+    function nextQuestion () {
+    questionCounter++;
+    questionText.innerText = questions[questionCounter].question;
+            var answerButton = document.createElement("button");
+            answerButton.className = "answer-choice btn";
+             answerButton.innerText = questions[questionCounter].answers[i].text;
+             answerButton.value = questions[questionCounter].answers[i].correct;
+             answerSet.appendChild(answerButton);
+            };
+            
+            
+            
+            
+            var startGame = function () {
+                console.log("hi");
+                startButtonEl.classList.add("hide");
+                questionSpace.classList.remove("hide");
+                showTimer();
+                showQuestion();
+                
+                
+            }
+            
 
-
-//build function to increase index number
-
-// questionText.innerText = questions[0].question;
-// answerSet.innerHTML = questions[0].answers.forEach(answers => {
-//     const button = document.createElement("button");
-//     button.appendChild;
-//     button.innerText = answers.text});
-// };
-
-
-var startGame = function () {
-    console.log("hi");
-    startButtonEl.classList.add("hide");
-    questionSpace.classList.remove("hide");
-    showQuestion();
-    showTimer();
-
-}
-
-
-var showHighScores = function () {
-    var highScoreList = document.getElementById("")
-}
 
 /*GIVEN I am taking a code quiz
 WHEN I click the Start button*/
 startButtonEl.addEventListener("click", startGame);
+
+/*WHEN the game is over
+THEN I can save my initials and score*/
+
+
 /*
 WHEN I answer a question
-THEN I am presented with another question
-
-if answer:'correct' (eventListener)clicked = false, timerEl (countdown) decrements by 15 secs.
-
-
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and score*/
-//localStorage.setItem (user, highscore)
-//JSON.parse(window.localStorage.getItem(highscores)) || []
-// JSON parse. remember that localStorage only reads strings
-// var highscore = []
+THEN I am presented with another question*/
